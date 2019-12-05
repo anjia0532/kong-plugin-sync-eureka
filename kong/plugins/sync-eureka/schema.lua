e9a13c7745561cb96ca6056220752b4640bc9186
anjia0532 <anjia0532@gmail.com>
2019-12-05 12:39:03 +0800

    sync_interval change to 30 sec

--- kong/plugins/sync-eureka/schema.lua
+++ kong/plugins/sync-eureka/schema.lua
@@ -6,7 +6,7 @@ return {
                 type = "record",
                 fields = {
                     {eureka_url = {type = "string"}},
-                    {sync_interval = {type = "number", default = 10}},
+                    {sync_interval = {type = "number", default = 30}},
                     {clean_target_interval = {type = "number", default = 86400}}
                 }
             }
